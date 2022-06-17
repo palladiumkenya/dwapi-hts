164ea2799c1a91e1807f549b961b48b51e4941df
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-06-17 09:55:23 +0300

    updated manifest dto with session

--- src/Dwapi.Hts.Core/Exchange/ManifestDto.cs
+++ src/Dwapi.Hts.Core/Exchange/ManifestDto.cs
@@ -16,6 +16,10 @@ namespace Dwapi.Hts.Core.Exchange
         public DateTime BuildDate { get; set; }
         public int PatientCount { get; set; }
         public string Cargo { get; set; }
+        public Guid? Session { get; set; }
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+        public string Tag { get; set; }
 
         public ManifestDto(Manifest manifest, int count)
         {
@@ -26,6 +30,10 @@ namespace Dwapi.Hts.Core.Exchange
             LogDate = manifest.DateLogged;
             BuildDate = manifest.DateArrived;
             PatientCount = count;
+            Session = manifest.Session;
+            Start = manifest.Start;
+            End = manifest.End;
+            Tag = manifest.Tag;
             var cargoes = manifest.Cargoes.Where(x => x.Type != CargoType.Patient).ToList();
             if(cargoes.Any())
                 Cargo = JsonConvert.SerializeObject(cargoes);
