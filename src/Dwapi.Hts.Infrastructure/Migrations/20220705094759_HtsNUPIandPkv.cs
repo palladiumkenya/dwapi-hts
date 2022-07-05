f71e3c752f15f809dd0d4c072bf51b7110a27421
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-07-05 13:29:56 +0300

    added NUPI and Pkv

---
+++ src/Dwapi.Hts.Infrastructure/Migrations/20220705094759_HtsNUPIandPkv.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dwapi.Hts.Infrastructure.Migrations
+{
+    public partial class HtsNUPIandPkv : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NUPI",
+                table: "Clients",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pkv",
+                table: "Clients",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NUPI",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Pkv",
+                table: "Clients");
+        }
+    }
+}
