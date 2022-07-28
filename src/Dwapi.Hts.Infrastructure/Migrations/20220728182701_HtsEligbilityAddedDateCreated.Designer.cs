8925f8fce3a5468b3fe68a93f70f263b3ff80959
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-07-29 00:02:06 +0300

    added new eligibility fields

--- src/Dwapi.Hts.Infrastructure/Migrations/20220728182701_HtsEligbilityAddedDateCreated.Designer.cs
+++ src/Dwapi.Hts.Infrastructure/Migrations/20220728182701_HtsEligbilityAddedDateCreated.Designer.cs
@@ -4,14 +4,16 @@ using Dwapi.Hts.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dwapi.Hts.Infrastructure.Migrations
 {
     [DbContext(typeof(HtsContext))]
-    partial class HtsContextModelSnapshot : ModelSnapshot
+    [Migration("20220728182701_HtsEligbilityAddedDateCreated")]
+    partial class HtsEligbilityAddedDateCreated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
