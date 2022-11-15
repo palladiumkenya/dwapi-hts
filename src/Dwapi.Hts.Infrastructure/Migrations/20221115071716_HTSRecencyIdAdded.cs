4198aaddc43757b9b64bc515278c64ecd91d695f
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-11-15 10:36:43 +0300

    added recency id

---
+++ src/Dwapi.Hts.Infrastructure/Migrations/20221115071716_HTSRecencyIdAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dwapi.Hts.Infrastructure.Migrations
+{
+    public partial class HTSRecencyIdAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HtsRecencyId",
+                table: "Clients",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HtsRecencyId",
+                table: "Clients");
+        }
+    }
+}
